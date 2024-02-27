869b047ac6f30d6dd4005b15531c1834e4eb0171
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-02-28 04:00:54 +0530

    creating a custom button and using it

--- GuessMyNumber/screens/StartGameScreen.js
+++ GuessMyNumber/screens/StartGameScreen.js
@@ -1,6 +1,13 @@
-import {TextInput} from 'react-native';
-function StartGameScreen(){
-
+import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
 }
 
 export default StartGameScreen;
