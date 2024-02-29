4ab8dea1c51c022ce6bb044fbe658e032892c1e0
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-01 04:53:00 +0530

    adding styles to input area for numbers

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2}/>
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -20,9 +20,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#72063c",
     borderRadius: 8,
     elevation: 4,
-    shadowColor:'black',
-    shadowOpacity:0.5,
-    shadowOffset:{width:0,height:2},
-    shadowRadius:6
+    shadowColor: "black",
+    shadowOpacity: 0.5,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    fontWeight: "bold",
+    marginVertical: 8,
+    textAlign:'center'
   },
 });
