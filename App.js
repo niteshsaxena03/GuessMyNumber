156e8ea52e52e25d099b095196ef3b4a04c08ecd
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-01 04:44:31 +0530

    adding some styling to buttons for andoird & iOS

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+  
       <StartGameScreen/>
-    </View>
+ 
   );
 }
 
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,8 +1,8 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -11,3 +11,18 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 24,
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor:'black',
+    shadowOpacity:0.5,
+    shadowOffset:{width:0,height:2},
+    shadowRadius:6
+  },
+});
