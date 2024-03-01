c1984896d05e8d144c3908be565e59164ace4f1d
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-01 05:57:15 +0530

    adding a background colour to root screen

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-  
-      <StartGameScreen/>
- 
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  rootScreen: {
+    flex:1,
+    backgroundColor: "#ddb52f",
   },
 });
