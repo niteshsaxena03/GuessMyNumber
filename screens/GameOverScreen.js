cf289f83f271c03c3c15542e6374c752ecb20f54
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-09 08:28:44 +0530

    adding styling to various components used in GameOverScreen and also using a foreground image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,42 @@
-import { Text } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
+import Title from "../components/UI/Title";
+import Colors from "../constants/colors";
 
 function GameOverScreen() {
-  return <Text>Game is over!</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y</Text>
+    </View>
+  );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 350,
+    height: 350,
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 26,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
