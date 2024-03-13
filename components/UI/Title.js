a9abb32ef6e0ad30b567e03360553e8a6d0e82fd
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-14 04:39:58 +0530

    adding different styles for android and iOS using the Platform API

--- components/UI/Title.js
+++ components/UI/Title.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Platform } from "react-native";
 import Colors from "../../constants/colors";
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -8,13 +8,13 @@ export default Title;
 
 const styles = StyleSheet.create({
   title: {
-  
     marginTop: 12,
     fontSize: 24,
 
     color: Colors.accent500,
     textAlign: "center",
-    borderWidth: 2,
+    //borderWidth: Platform.OS==='android'?2:0,
+    borderWidth: Platform.select({ ios: 0, android: 2 }),
     borderColor: Colors.accent500,
     padding: 12,
     maxWidth: "80%",
